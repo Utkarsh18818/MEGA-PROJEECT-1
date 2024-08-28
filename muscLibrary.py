cd14227fcc94a67b405fc97939f970e5915d53f6
Utkarsh <Utkarsh190707@gmail.com>
2024-08-28 06:35:24 +0530

    self cusstomised music liberary

---
+++ muscLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "Arcade":"https://www.youtube.com/watch?v=51u5fnyrGj4",
+    "Closer":"https://www.youtube.com/watch?v=PT2_F-1esPk",
+    "See you again":"https://www.youtube.com/watch?v=NDEWXnMRq3c",
+    "happier":"https://www.youtube.com/watch?v=m7Bc3pLyij0",
+
+}
