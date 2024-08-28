856f38a74e6adea95fc6ec697d380ba54650ea5f
Utkarsh <Utkarsh190707@gmail.com>
2024-08-28 06:35:50 +0530

    api data done

---
+++ 90.py
@@ -0,0 +1,63 @@
+ #"status" :"ok",
+#"totalResults": 36,
+#-"articles": [
+-{
+-"source": {
+"id": "the-washington-post",
+"name": "The Washington Post"
+},
+"author": "Amy B Wang, Mariana Alfaro, Azi Paybarah, Patrick Svitek, Maegan Vazquez, Isaac Arnsdorf, Meryl Kornfield, Will Oremus, Marianne LeVine, Caroline Kitchener, Leigh Ann Caldwell, Theodoric Meyer, Yasmeen Abutaleb, Marianna Sotomayor, Jacqueline Alemany, Missy Ryan, Michael Birnbaum, Emily Rauhala, Ellen Nakashima",
+"title": "Election 2024 latest news: Pelosi urges Biden to make a decision on future. He says he already has. - The Washington Post",
+"description": "Live updates from the 2024 campaign trail with the latest news on presidential candidates, polls, primaries and more.",
+"url": "https://www.washingtonpost.com/politics/2024/07/10/election-2024-campaign-updates/",
+"urlToImage": "https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/HVVCFTM3J4P6KCCLZDDJ6HMOTU.JPG&w=1440",
+"publishedAt": "2024-07-10T14:41:18Z",
+"content": null
+},
+-{
+-"source": {
+"id": "the-wall-street-journal",
+"name": "The Wall Street Journal"
+},
+"author": "The Wall Street Journal",
+"title": "First F-16 Jet Fighters on Their Way to Ukraine, U.S. and Allies Say - The Wall Street Journal",
+"description": null,
+"url": "https://www.wsj.com/world/first-f-16-jet-fighters-on-their-way-to-ukraine-u-s-and-allies-say-943b9ba4",
+"urlToImage": null,
+"publishedAt": "2024-07-10T13:36:00Z",
+"content": null
+},
+-{
+-"source": {
+"id": "cnn",
+"name": "CNN"
+},
+"author": "Stephen Collinson",
+"title": "Biden buys time but Democratic senator warns he could lose in ‘landslide’ - CNN",
+"description": "Joe Biden’s refusal to step aside stifled attempts to push him off the Democratic ticket on Tuesday.",
+"url": "https://www.cnn.com/2024/07/10/politics/biden-democratic-lawmakers-analysis/index.html",
+"urlToImage": "https://media.cnn.com/api/v1/images/stellar/prod/2024-07-07t223233z-38326351-rc2mq8aa70ab-rtrmadp-3-usa-election-biden.jpg?c=16x9&q=w_800,c_fill",
+"publishedAt": "2024-07-10T13:19:00Z",
+"content": "President Joe Biden on Tuesday clung onto the Democratic nomination on a high-stakes day in Washington, but the risks of his refusal to step aside were laid bare when one of his partys senators warne… [+11989 chars]"
+},
+-{
+-"source": {
+"id": "bloomberg",
+"name": "Bloomberg"
+},
+"author": null,
+"title": "Intuit to Cut 10% of Employees in Latest Round of Tech Layoffs - Bloomberg",
+"description": null,
+"url": "https://www.bloomberg.com/news/articles/2024-07-10/intuit-to-cull-10-of-employees-in-latest-round-of-tech-layoffs",
+"urlToImage": null,
+"publishedAt": "2024-07-10T13:15:39Z",
+"content": "To continue, please click the box below to let us know you're not a robot."
+},
+-{
+-"source": {
+"id": "reuters",
+"name": "Reuters"
+},
+"author": "Reuters",
+"title": "Gaza airstrike hit as displaced gathered for soccer match, witnesses say - Reuters",
+"description": null,
